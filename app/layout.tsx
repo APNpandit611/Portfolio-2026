436fb85a548ee847cfcbaa64ebdd04276743c197
APNpandit611 <apnpanditt@gmail.com>
2026-01-07 08:12:33 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,14 @@ import Footer from "@/components/Footer";
 export const metadata: Metadata = {
     title: "Portfolio",
     description: "A personal portfolio",
+    icons: {
+        apple: "/apple-touch-icon.png",
+        icon: [
+            { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+            { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+        ],
+    },
+    manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
